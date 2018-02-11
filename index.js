142390ee54a239871459739b82b2ca7829d6a705
George  Pianka <gpianka1@gmail.com>
2018-02-11 11:16:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = [];
   for (var i=0; i<4; i++) {
-    emptyArray.push (`${musicians} plays ${instruments}`);
+    emptyArray.push (`${musicians[i]} plays ${instruments[i]}`);
   }
   return emptyArray;
 }
